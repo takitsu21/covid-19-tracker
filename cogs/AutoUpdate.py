1b58dd4e90f1b033b906f004f6e458c021d78e20
takitsu21 <dylannbatisse@gmail.com>
2020-03-05 00:12:37 +0100

    string formatting and data parsing

--- cogs/AutoUpdate.py
+++ cogs/AutoUpdate.py
@@ -19,14 +19,16 @@ class AutoUpdater(commands.Cog):
         self.bot = bot
         self.bot.loop.create_task(self.main())
 
-    def _csv_update(self) -> None:
-        with requests.Session() as s:
-            for uri, fpath in utils.DICT.items():
-                download = s.get(uri)
-                decoded_content = download.content.decode('utf-8')
-                with open(fpath, "w") as f:
-                    f.write(decoded_content)
-        logger.info("csv downloaded")
+    def cache(self) -> None:
+        utils.cache_data(utils.URI_DATA)
+
+    async def send_notifications(self, channels_id):
+        for id in channels_id:
+            channel = self.bot.get_channel(id)
+            embed = discord.Embed()
+            await channel.send(embed=embed)
+        logger.info("Notifications sended")
+        pass
 
     def diff_checker(self, csv_data: List[dict]) -> bool:
         """
@@ -39,18 +41,23 @@ class AutoUpdater(commands.Cog):
             return utils.last_key(csv_data) == utils.last_key(cr)
         raise requests.RequestException(f"Request error : {r.status_code}")
 
+    async def update(self, channels_id):
+        self.cache()
+        logger.info("New data downloaded")
+        plot_csv()
+        logger.info("New plot generated")
+        await self.send_notifications(channels_id)
+
     async def main(self):
         while True:
-            if not os.path.exists(utils._CONFIRMED_PATH):
-                self._csv_update()
-                plot_csv()
-                logger.info("data has been plotted")
-            if self.diff_checker(utils.data_reader(utils._CONFIRMED_PATH)):
-                logger.info("csv are up to date")
+            if not os.path.exists(utils.DATA_PATH):
+                channels_id = []
+                await self.update(channels_id)
+            if self.diff_checker(utils.data_reader(utils.DATA_PATH)):
+                logger.info("Datas are up to date")
             else:
-                self._csv_update()
-                plot_csv()
-                logger.info("data has been plotted")
+                channels_id = []
+                await self.update(channels_id)
             await asyncio.sleep(3600)
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 import csv
-from typing import List, Dict
+from typing import List, Dict, Tuple
 import functools
 from discord.ext import commands
 import datetime as dt
@@ -7,36 +7,68 @@ import time
 import os
 import requests
 from data.datas import DATA
+from decouple import config
 
 
-URI_DATA = "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=Confirmed%20%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Confirmed%20desc%2CCountry_Region%20asc%2CProvince_State%20asc&resultOffset=0&resultRecordCount=250&cacheHint=true"
-# URI_DATA = config("uri_data")
-_CONFIRMED_URI = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
-_DEATH_URI = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
-_RECOVERED_URI = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
+URI_DATA      = config("uri_data")
+DATA_PATH     = "data/datas.py"
+COLOR         = 0x5A12DF
+DISCORD_LIMIT = 2 ** 11 # discord limit
+USER_AGENT    = {'User-Agent': 'Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/73.0'}
 
-_CONFIRMED_PATH = "data/time_series_19-covid-Confirmed.csv"
-_DEATH_PATH = "data/time_series_19-covid-Deaths.csv"
-_RECOVERED_PATH = "data/time_series_19-covid-Recovered.csv"
-
-DICT = {
-    _CONFIRMED_URI: _CONFIRMED_PATH,
-    _DEATH_URI: _DEATH_PATH,
-    _RECOVERED_URI: _RECOVERED_PATH
-    }
-
-COLOR = 0x5A12DF
 
 def cache_data(uri):
-    r = requests.get(URI_DATA)
-    with open("data/datas.py", "w") as f:
-        f.write("DATA = {}".format(r.json()))
+    r = requests.get(URI_DATA, headers=USER_AGENT)
+    if r.status_code >= 200 and r.status_code <= 299:
+        with open("data/datas.py", "w") as f:
+            f.write("DATA = {}".format(parse_data(r.json())))
 
-def te(data: dict):
-    features = data["features"]
-    for f in features:
-        print(f)
-# def g
+def parse_data(data):
+    d = {}
+    for iter in data["features"]:
+        f = iter["attributes"]
+        if f["Country_Region"] not in d:
+            d[f["Country_Region"]] = {
+                "confirmed": f["Confirmed"],
+                "recovered": f["Recovered"],
+                "deaths": f["Deaths"]
+            }
+        else:
+            d[f["Country_Region"]]["confirmed"] += f["Confirmed"]
+            d[f["Country_Region"]]["recovered"] += f["Recovered"]
+            d[f["Country_Region"]]["deaths"] += f["Deaths"]
+    return d
+
+def string_formatting(data_parsed: dict) -> Tuple[str, str]:
+    total_confirmed = 0
+    total_recovered = 0
+    total_deaths = 0
+    max_length = DISCORD_LIMIT - 50
+    old_text = ""
+    text = ""
+    d = {}
+    header = "Total confirmed **{}**\nTotal recovered **{}** ({})\nTotal deaths **{}** ({})"
+    length = len(header)
+    basic_length = 16
+    for k, v in data_parsed.items():
+        length += len(str(k)) + len(str(v['confirmed'])) + basic_length
+        total_confirmed += v['confirmed']
+        total_recovered += v['recovered']
+        total_deaths += v['deaths']
+        if length < max_length:
+            text += f"**{k}** {v['confirmed']} Confirmed\n"
+            old_text = text
+
+    if length > max_length:
+        text = old_text + "...Too many country to show"
+    header = header.format(
+        total_confirmed,
+        total_recovered,
+        percentage(total_confirmed, total_recovered),
+        total_deaths,
+        percentage(total_confirmed, total_deaths)
+    )
+    return header, text
 
 def trigger_typing(func):
     @functools.wraps(func)
@@ -56,65 +88,65 @@ def discord_timestamp():
 def last_key(csv_data: List[dict]) -> int:
     return list(csv_data[0].keys())[-1]
 
-def last_update():
-    last_csv_update = dt.datetime.utcfromtimestamp(os.path.getctime(_CONFIRMED_PATH))
-    return f"Last update {last_csv_update.month}-{last_csv_update.day}-{last_csv_update.year} {last_csv_update.hour:0<2}:{last_csv_update.minute:0<2} GMT"
+def last_update(fpath: str):
+    last_csv_update = dt.datetime.utcfromtimestamp(os.path.getctime(fpath))
+    return f"Last update {last_csv_update.month}-{last_csv_update.day}-{last_csv_update.year} {last_csv_update.hour:0<2}:{last_csv_update.minute} GMT"
 
 def percentage(total, x):
     return "{:.2f}%".format(x * 100 / total)
 
-def format_csv(csv_confirmed, csv_recovered, csv_deaths) -> Dict[str, int]:
-    lk = last_key(csv_confirmed)
-    res = {}
-    total_confirmed = 0
-    total_deaths = 0
-    total_recovered = 0
-    for confirmed in csv_confirmed:
-        if confirmed["Country/Region"] not in res:
-            res[confirmed["Country/Region"]] = {}
-            res[confirmed["Country/Region"]]["confirmed"] = int(confirmed[lk])
-        else:
-            res[confirmed["Country/Region"]]["confirmed"] += int(confirmed[lk])
-        total_confirmed += int(confirmed[lk])
+# def format_csv(csv_confirmed, csv_recovered, csv_deaths) -> Dict[str, int]:
+#     lk = last_key(csv_confirmed)
+#     res = {}
+#     total_confirmed = 0
+#     total_deaths = 0
+#     total_recovered = 0
+#     for confirmed in csv_confirmed:
+#         if confirmed["Country/Region"] not in res:
+#             res[confirmed["Country/Region"]] = {}
+#             res[confirmed["Country/Region"]]["confirmed"] = int(confirmed[lk])
+#         else:
+#             res[confirmed["Country/Region"]]["confirmed"] += int(confirmed[lk])
+#         total_confirmed += int(confirmed[lk])
 
-    for recovered in csv_recovered:
-        if "recovered" not in res[recovered["Country/Region"]]:
-            res[recovered["Country/Region"]]["recovered"] = int(recovered[lk])
-        else:
-            res[recovered["Country/Region"]]["recovered"] += int(recovered[lk])
-        total_recovered += int(recovered[lk])
+#     for recovered in csv_recovered:
+#         if "recovered" not in res[recovered["Country/Region"]]:
+#             res[recovered["Country/Region"]]["recovered"] = int(recovered[lk])
+#         else:
+#             res[recovered["Country/Region"]]["recovered"] += int(recovered[lk])
+#         total_recovered += int(recovered[lk])
 
-    for deaths in csv_deaths:
-        if "deaths" not in res[deaths["Country/Region"]]:
-            res[deaths["Country/Region"]]["deaths"] = int(deaths[lk])
-        else:
-            res[deaths["Country/Region"]]["deaths"] += int(deaths[lk])
-        total_deaths += int(deaths[lk])
+#     for deaths in csv_deaths:
+#         if "deaths" not in res[deaths["Country/Region"]]:
+#             res[deaths["Country/Region"]]["deaths"] = int(deaths[lk])
+#         else:
+#             res[deaths["Country/Region"]]["deaths"] += int(deaths[lk])
+#         total_deaths += int(deaths[lk])
 
-    res["total"] = {
-        "total_confirmed": total_confirmed,
-        "total_recovered": total_recovered,
-        "total_deaths": total_deaths
-    }
-    return res
+#     res["total"] = {
+#         "total_confirmed": total_confirmed,
+#         "total_recovered": total_recovered,
+#         "total_deaths": total_deaths
+#     }
+#     return res
 
-def string_formatting(formatted_data: Dict[str, int], param=None) -> str:
-    total = formatted_data['total']
-    p_r = percentage(total['total_confirmed'], total['total_recovered'])
-    p_d = percentage(total['total_confirmed'], total['total_deaths'])
-    header = f"Total confirmed : **{total['total_confirmed']}**\nTotal recovered : **{total['total_recovered']}** ({p_r})\nTotal deaths : **{total['total_deaths']}** ({p_d})\n"
-    res = ""
-    del formatted_data["total"]
-    if param is not None:
-        for country, data in formatted_data.items():
-            if country.lower().startswith(param):
-                res += f"**{country}** : {data['confirmed']} confirmed, {data['recovered']} recovered, {data['deaths']} deaths\n"
-    else:
-        for country, data in formatted_data.items():
-            res += f"**{country}** : {data['confirmed']} Confirmed\n"
-    return header, res
+# def string_formatting(formatted_data: Dict[str, int], param=None) -> str:
+#     total = formatted_data['total']
+#     p_r = percentage(total['total_confirmed'], total['total_recovered'])
+#     p_d = percentage(total['total_confirmed'], total['total_deaths'])
+#     header = f"Total confirmed : **{total['total_confirmed']}**\nTotal recovered : **{total['total_recovered']}** ({p_r})\nTotal deaths : **{total['total_deaths']}** ({p_d})\n"
+#     res = ""
+#     del formatted_data["total"]
+#     if param is not None:
+#         for country, data in formatted_data.items():
+#             if country.lower().startswith(param):
+#                 res += f"**{country}** : {data['confirmed']} confirmed, {data['recovered']} recovered, {data['deaths']} deaths\n"
+#     else:
+#         for country, data in formatted_data.items():
+#             res += f"**{country}** : {data['confirmed']} Confirmed\n"
+#     return header, res
 
 if __name__ == "__main__":
-    # cache_data(URI_DATA)
     # print(DATA["objectIdFieldName"])
-    te(DATA)
+    # cache_data(URI_DATA)
+    print(string_formatting(DATA))
